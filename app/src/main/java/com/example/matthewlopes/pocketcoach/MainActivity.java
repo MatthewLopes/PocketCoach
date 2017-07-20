4608d4d9a6781d3dc1706aa4e3c9e1dc808140f2
calebheinzman <caleb.heinzman@gmail.com>
2017-07-19 20:31:03 -0400

    Navigation

--- app/src/main/java/com/example/matthewlopes/pocketcoach/MainActivity.java
+++ app/src/main/java/com/example/matthewlopes/pocketcoach/MainActivity.java
@@ -9,76 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-/*//Beginning of added section
-    //Setting up listeners
-  public class MainActivity extends Activity {
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-            bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-                @Override
-                public void onTabSelected(@IdRes int tabId) {
-                    if (tabId == R.id.tab_favorites) {
-                        // The tab with id R.id.tab_favorites was selected,
-                        // change your content accordingly.
-                    }
-                }
-            });
-        }
-
-
-  bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
-        @Override
-        public void onTabReSelected(@IdRes int tabId) {
-            if (tabId == R.id.tab_favorites) {
-                // The tab with id R.id.tab_favorites was reselected,
-                // change your content accordingly.
-            }
-        }
-    })
-
-    //Intercepting tab selections
-    bottomBar.setTabSelectionInterceptor(new TabSelectionInterceptor() {
-    @Override
-    public boolean shouldInterceptTabSelection(@IdRes int oldTabId, @IdRes int newTabId) {
-        if (newTabId == R.id.tab_pro_feature && !userHasProVersion()) {
-          startProVersionPurchaseFlow();
-          return true;
-        }
-
-        return false;
-    }
-});
-
-    //Changing icons based on selection state
-<selector xmlns:android="http://schemas.android.com/apk/res/android">
-    <item android:drawable="@drawable/ic_myicon_selected" android:state_selected="true" />
-    <item android:drawable="@drawable/ic_myicon_default" android:state_selected="false" />
-</selector>
-
-    //Those color changing tabs look dope. Howdoidodat?
-<tabs>
-    <tab
-        id="@+id/tab_favorites"
-        icon="@drawable/ic_favorites"
-        title="Favorites"
-        barColorWhenSelected="#5D4037" />
-    <!-- You can use @color resources too! -->
-</tabs>
-
-    //Finally, set bb_behavior to include the underNavbar flag and you're good to go!
-<com.roughike.bottombar.BottomBar
-    android:id="@+id/bottomBar"
-    android:layout_width="match_parent"
-    android:layout_height="56dp"
-    android:layout_alignParentBottom="true"
-    app:bb_tabXmlResource="@xml/my_awesome_bottombar_tabs"
-    app:bb_behavior="underNavbar" />
-
-    //End of added section */
 
     private TextView mTextMessage;
 
@@ -88,14 +18,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_social:
+                    setContentView(R.layout.social);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_workout:
+                    setContentView(R.layout.workout);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    setContentView(R.layout.activity_main);
+                    return true;
+                case R.id.navigation_data:
+                    setContentView(R.layout.data);
+                    return true;
+                case R.id.navigation_gyms:
+                    setContentView(R.layout.workout);
                     return true;
             }
             return false;
