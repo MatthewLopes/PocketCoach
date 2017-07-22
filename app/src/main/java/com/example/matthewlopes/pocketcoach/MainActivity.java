81b57dc44b33d18c448bfe5d4e40b6f145ac9877
calebheinzman <caleb.heinzman@gmail.com>
2017-07-21 21:41:23 -0400

    ***Finished Bottom Navigation***
    What I did:
    Replaced all activities with "fragments". (Fragments allow me to swap from view to view but keep the same bottom navigation features.

--- app/src/main/java/com/example/matthewlopes/pocketcoach/MainActivity.java
+++ app/src/main/java/com/example/matthewlopes/pocketcoach/MainActivity.java
@@ -1,42 +1,56 @@
 package com.example.matthewlopes.pocketcoach;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import layout.DataFragment;
+import layout.GymFragment;
+import layout.ProfileFragment;
+import layout.SocialFragment;
+import layout.WorkoutFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView mTextMessage;
+    //private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+
+    //This method listens for which navigation item is clicked, then switchs the fragment to the
+    //Corresponding fragment.
+    public BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentManager manager = getSupportFragmentManager();
+            manager = getSupportFragmentManager();
             switch (item.getItemId()) {
                 case R.id.navigation_social:
-                    setContentView(R.layout.social);
+                    SocialFragment socialFragment = new SocialFragment();
+                    manager.beginTransaction().replace(R.id.contentLayout, socialFragment, socialFragment.getTag()).commit();
                     return true;
                 case R.id.navigation_workout:
-                    setContentView(R.layout.workout);
+                    WorkoutFragment workoutFragment = new WorkoutFragment();
+                    manager.beginTransaction().replace(R.id.contentLayout, workoutFragment, workoutFragment.getTag()).commit();
                     return true;
                 case R.id.navigation_profile:
-                    setContentView(R.layout.activity_main);
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    manager.beginTransaction().replace(R.id.contentLayout, profileFragment, profileFragment.getTag()).commit();
                     return true;
                 case R.id.navigation_data:
-                    setContentView(R.layout.data);
+                    DataFragment dataFragment = new DataFragment();
+                    manager.beginTransaction().replace(R.id.contentLayout, dataFragment, dataFragment.getTag()).commit();
                     return true;
                 case R.id.navigation_gyms:
-                    setContentView(R.layout.workout);
+                    GymFragment gymFragment = new GymFragment();
+                    manager.beginTransaction().replace(R.id.contentLayout, gymFragment, gymFragment.getTag()).commit();
                     return true;
             }
             return false;
         }
-
     };
 
     @Override
@@ -44,8 +58,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //This code sets the default starting fragment to the profile page so that the first page
+        //The user sees is the profile page.
+        android.support.v4.app.FragmentManager manager = getSupportFragmentManager();
+        manager = getSupportFragmentManager();
+        ProfileFragment profileFragment = new ProfileFragment();
+        manager.beginTransaction().replace(R.id.contentLayout, profileFragment, profileFragment.getTag()).commit();
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_profile);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
